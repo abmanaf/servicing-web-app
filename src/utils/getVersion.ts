b89adf44fba17012f8bf536123b3811bec48adcb
abmanaf <aliu.manaf@amalitech.com>
2025-08-30 20:53:09 +0000

    chore: update the version to remove test mode

--- src/utils/getVersion.ts
+++ src/utils/getVersion.ts
@@ -1,5 +1,9 @@
+export const isInVisualEditor = typeof window !== 'undefined' && window.location !== window.parent.location;
+
 export default function getVersion() {
-  if (process.env.NODE_ENV === 'development' || process.env.STORYBLOK_IS_PREVIEW === 'true') {
+  if (process.env.NODE_ENV === 'development' || 
+      process.env.STORYBLOCK_IS_PREVIEW === 'true' || 
+      isInVisualEditor) {
     return 'draft';
   }
   return 'published';
