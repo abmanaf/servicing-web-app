3a885073468d16204f16fe1cde05e1f36ce4ba7f
abmanaf <aliu.manaf@amalitech.com>
2025-08-30 11:58:31 +0000

    chore: improve preview storyblok hanlding

--- src/lib/storyblok.ts
+++ src/lib/storyblok.ts
@@ -1,18 +1,14 @@
-import Page from "@/components/page";
-import HeroSection from "@/components/molecule/hero";
 import { apiPlugin, storyblokInit } from "@storyblok/react/rsc";
+import { componentMap } from "./componentMap";
 
-const isPreview = process.env.STORYBLOK_IS_PREVIEW === "true";
+const isPreview = process.env.STORYBLOK_IS_PREVIEW === "true" ? true : false;
 const accessToken = process.env.STORYBLOK_TOKEN || process.env.STORYBLOK_PUBLIC_TOKEN;
 
 export const getStoryblokApi = storyblokInit({
   bridge: isPreview,
   accessToken,
   use: [apiPlugin],
-  components: {
-    page: Page,
-    heroSection: HeroSection,
-  },
+  components: componentMap,
   apiOptions: {
     region: "eu",
   },
