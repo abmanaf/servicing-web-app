baaa1817aef2a6fdfab7e803df007864ec56dbb0
abmanaf <aliu.manaf@amalitech.com>
2025-09-10 13:35:54 +0000

    feat: update componentMap to include SharedHeroSection, StatsSection, AboutSummary, and OurTeam components; replace ServicesHeroSection with otherHeroSection for improved modularity

--- src/lib/componentMap.ts
+++ src/lib/componentMap.ts
@@ -4,9 +4,12 @@ import { ProviderSection } from "@/components/molecules/home/provider-section";
 import WhyChooseUs from "@/components/molecules/home/why-choose-us";
 import { OurServices } from "@/components/molecules/home/our-services.tsx";
 import { HappyCustomer } from "@/components/molecules/home/happy-customer";
-import { ServicesHeroSection } from "@/components/molecules/services/hero-section";
+import { SharedHeroSection } from "@/components/molecules/shared/hero";
 import { ServicesSection } from "@/components/molecules/services/services-section";
 import { WorkProcess } from "@/components/molecules/services/work-process";
+import { StatsSection } from "@/components/molecules/about/stats-section";
+import { AboutSummary } from "@/components/molecules/about/summary";
+import { OurTeam } from "@/components/molecules/about/our-team";
 
 export const componentMap = {
   page: Page,
@@ -15,7 +18,10 @@ export const componentMap = {
   whyChooseUs: WhyChooseUs,
   ourSevices: OurServices,
   happyCustomer: HappyCustomer,
-  servicesHeroSection: ServicesHeroSection,
+  otherHeroSection: SharedHeroSection,
   servicesSection: ServicesSection,
   workProcess: WorkProcess,
+  statsSection: StatsSection,
+  aboutSummary: AboutSummary,
+  ourTeam: OurTeam,
 };
