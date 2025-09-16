52cf4b4205fd6d459c709b754099ca55e03803a3
abmanaf <aliu.manaf@amalitech.com>
2025-09-16 19:36:32 +0000

    feat: add Vercel Analytics integration for enhanced performance tracking in the application

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 import StoryblokProvider from "@/components/StoryblokProvider";
+import { Analytics } from "@vercel/analytics/next";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -28,7 +29,10 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <StoryblokProvider>{children}</StoryblokProvider>
+        <StoryblokProvider>
+          {children}
+          <Analytics />
+        </StoryblokProvider>
       </body>
     </html>
   );
